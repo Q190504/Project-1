ca4dea22e7ca080c148ad019c36283649fc659df
Pauline <quynh190504@gmail.com>
2025-02-23 10:29:13 +0700

    Add player control

---
+++ Assets/Scripts/EventSystem/TakeDamage/TakeDamagePublisherSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New TakeDamage Pulisher", menuName = "Scriptable Objects/Events/TakeDamage Publisher")]
+public class TakeDamagePublisherSO : ScriptableObject
+{
+    public UnityAction<float, string, int> OnEventRaised;
+
+    public void RaiseEvent(float value, string tag, int myName)
+    {
+        OnEventRaised?.Invoke(value, tag, myName);
+    }
+}
---
+++ Assets/Scripts/Units/Health/BaseHealth.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BaseHealth : MonoBehaviour
+{
+    [SerializeField] protected float currentHealth;
+    [SerializeField] protected float maxHealth;
+    [SerializeField] protected StringPublisherSO onDie;
+    public virtual void TakeDmg(float dmg)
+    {
+        currentHealth -= dmg;
+    }
+
+    public virtual void Sacrifice(float remaining)
+    {
+        currentHealth = remaining;
+    }
+
+    public virtual void OnGetDamaged(float damage, string tag, int id)
+    {
+        if (id == this.gameObject.GetInstanceID() && tag != this.gameObject.tag)
+        {
+            TakeDmg(damage);
+        }
+    }
+
+    public virtual void Heal(float hp)
+    {
+        currentHealth += hp;
+    }
+
+    public virtual void Die()
+    {
+        onDie.RaiseEvent(this.gameObject.GetInstanceID().ToString());
+        // Destroy or disable player..
+    }
+}
