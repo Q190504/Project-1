ddb91b085cbaba6035c563c04e23528fcf2a52f3
Huynh Tran Khanh Quynh <quynh190504@gmail.com>
2025-03-16 11:26:06 +0700

    Adjust Slime Bullet Moving Logic
    Switch from using position to velocity

--- Assets/Scripts/Systems/Slime Bullet/SlimeBulletMoverSystem.cs
+++ Assets/Scripts/Systems/Slime Bullet/SlimeBulletMoverSystem.cs
@@ -7,24 +7,46 @@ using Unity.Physics;
 using Unity.Physics.Systems;
 using Unity.Jobs;
 using Unity.Collections;
+using UnityEngine.InputSystem;
 
 [BurstCompile]
 public partial struct SlimeBulletMoverSystem : ISystem
 {
+    private EntityManager entityManager;
+    private Entity player;
+
     public void OnUpdate(ref SystemState state)
     {
         var ecbSingleton = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>();
         var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
 
-        foreach (var (localTransform, slimeBulletComponent, entity) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<SlimeBulletComponent>>().WithEntityAccess())
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        if (!SystemAPI.TryGetSingletonEntity<PlayerTagComponent>(out player))
+        {
+            Debug.Log($"Cant Found Player Entity in SlimeBulletMoverSystem!");
+            return;
+        }
+
+        foreach (var (localTransform, slimeBulletComponent, physicsVelocity, entity) in SystemAPI.Query<RefRW<LocalTransform>, RefRW<SlimeBulletComponent>, RefRW<PhysicsVelocity>>().WithEntityAccess())
         {
             if (slimeBulletComponent.ValueRO.isAbleToMove)
             {
-                localTransform.ValueRW.Position += slimeBulletComponent.ValueRO.moveDirection * slimeBulletComponent.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
+                float2 targetVelocity = new float2(slimeBulletComponent.ValueRO.moveDirection.x, slimeBulletComponent.ValueRO.moveDirection.y) * slimeBulletComponent.ValueRO.moveSpeed;
+                physicsVelocity.ValueRW.Linear.xy = math.lerp(physicsVelocity.ValueRW.Linear.xy, targetVelocity, 0.1f);
+
                 slimeBulletComponent.ValueRW.distanceTraveled += slimeBulletComponent.ValueRO.moveSpeed * SystemAPI.Time.DeltaTime;
 
                 if (slimeBulletComponent.ValueRO.distanceTraveled >= slimeBulletComponent.ValueRO.maxDistance)
                     slimeBulletComponent.ValueRW.isAbleToMove = false;
+            }
+            else if(!slimeBulletComponent.ValueRO.isAbleToMove)
+            {
+                physicsVelocity.ValueRW.Linear.xy = 0;
+            }
+            else if(slimeBulletComponent.ValueRO.isBeingSummoned)
+            {
+                //return to player
+
             }
             else
             {
